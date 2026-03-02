c4a3cbe395136519533b22cf627f6612fb5ac0ca
adhamgaawan-max <adhamgaawan@gmail.com>
2026-03-02 09:15:03 +0000

    - task three main loop

--- taskthree.cpp
+++ taskthree.cpp
@@ -61,3 +61,53 @@ int simulateAging(const vector<int>& references, int numFrames) {
 
     return pageFaults;
 }
+
+// readReferences Function: reads a sequence of page references and stores them into a vector
+vector<int> readReferences(const string& filename) {
+    vector<int> references;
+    ifstream file(filename);
+
+    if (!file) {
+        cout << "Error opening file.\n";
+        return references;
+    }
+
+    int page;
+    while (file >> page) {
+        references.push_back(page);
+    }
+
+    file.close();
+    return references;
+}
+
+// Main Loop
+int main() {
+    string filename;
+    cout << "Enter reference file name: ";
+    cin >> filename;
+
+    vector<int> references = readReferences(filename);
+    if (references.empty()) {
+        cout << "No references loaded.\n";
+        return 1;
+    }
+
+    int maxFrames;
+    cout << "Enter maximum number of frames to test: ";
+    cin >> maxFrames;
+
+    cout << "\nFrames\tFaults per 1000 references\n";
+    cout << "--------------------------------------\n";
+
+    for (int frames = 1; frames <= maxFrames; frames++) {
+        int faults = simulateAging(references, frames);
+        double faultsPer1000 = (double)faults / references.size() * 1000;
+
+        cout << frames << "\t"
+             << fixed << setprecision(2)
+             << faultsPer1000 << endl;
+    }
+
+    return 0;
+}
